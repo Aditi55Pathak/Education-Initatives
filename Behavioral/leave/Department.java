aa96bbb347e1786c089ed0fd230cca19392a7ec8
Aditi Pathak <Aditi55Pathak>
2024-09-21 22:19:07 +0530

    behavioural

--- Behavioral/leave/Department.java
+++ Behavioral/leave/Department.java
@@ -1,6 +1,11 @@
+// Leave Application in College
+// Pattern: Observer (Behavioral)
+// Description: Notify different departments when a leave application is submitted.
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 interface Department {
     void update(String studentName);
 }
+
